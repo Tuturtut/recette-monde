f77e61735e3a36a67bf5f54d1d610e3cee266d0a
Arthur <arthursimonin88@gmail.com>
2025-05-22 11:51:48 +0200

    feat: Add popup inner elements

--- assets/script/script.js
+++ assets/script/script.js
@@ -53,6 +53,38 @@ function renderRecettesDetails(recipe) {
   console.log(recipe);
   let popup = document.getElementById("popup");
   popup.style.display = "flex";
+
+  let popupTitle = document.getElementById("popupTitle");
+  popupTitle.innerText = recipe.name;
+
+  let servings = document.getElementById("servings");
+  servings.innerText = "Nombre de personnes : " + recipe.servings;
+
+  let ingredients = document.getElementById("ingredients");
+  ingredients.innerText = "Ingrédients : ";
+
+  let ingredientList = document.getElementById("ingredientList");
+  ingredientList.innerHTML = "";
+
+  recipe.ingredients.map(({ unit = "", quantity = "", ingredient: name }) => {
+    const li = document.createElement("li");
+    li.innerText = unit
+      ? `${quantity} ${unit} de ${name}`
+      : `${quantity} ${unit} ${name}`;
+    ingredientList.appendChild(li);
+  });
+
+  let popupInstruction = document.getElementById("popupInstruction");
+  popupInstruction.innerText = "Contenu de la recette : " + recipe.description;
+
+  let appliance = document.getElementById("appliance");
+  appliance.innerText = "Outils : " + recipe.appliance;
+
+  let ustensils = document.getElementById("ustensils");
+  ustensils.innerText = "Ustensiles : " + recipe.ustensils.join(", ");
+
+  let time = document.getElementById("time");
+  time.innerText = "Temps de cuisson : " + recipe.time + " min";
 }
 
 function closePopup() {
