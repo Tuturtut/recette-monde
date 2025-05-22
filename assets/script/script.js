228a52ec87dceedbe909f48a9eee3589e2eaece0
Arthur <arthursimonin88@gmail.com>
2025-05-22 10:49:10 +0200

    feat: Add recipe to dom

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,3 +4,49 @@ async function fetchRecettes() {
 
   return data.recipes;
 }
+
+function renderRecettes(recipes) {
+  const recipesContainer = document.getElementById("recipesContainer");
+
+  // Create the article for each recipe
+
+  recipes.forEach((element) => {
+    console.log(element);
+    let recipeDiv = document.createElement("article");
+    recipeDiv.classList.add("recipe-card");
+
+    let recipeTitle = document.createElement("h2");
+    recipeTitle.innerText = element.name;
+
+    recipeDiv.appendChild(recipeTitle);
+
+    let peopleNumber = document.createElement("p");
+    peopleNumber.innerHTML = `<strong>Nombre de personnes : </strong>${element.servings}`;
+
+    recipeDiv.appendChild(peopleNumber);
+
+    let ingredientList = document.createElement("ul");
+
+    element.ingredients.forEach(
+      ({ unit = "", quantity = "", ingredient: name }) => {
+        const li = document.createElement("li");
+        li.innerText = unit
+          ? `${quantity} ${unit} de ${name}`
+          : `${quantity} ${unit} ${name}`;
+        ingredientList.appendChild(li);
+      }
+    );
+
+    recipeDiv.appendChild(ingredientList);
+
+    // Add article to container
+    recipesContainer.appendChild(recipeDiv);
+  });
+}
+
+async function main() {
+  const recettes = await fetchRecettes();
+  renderRecettes(recettes);
+}
+
+main();
